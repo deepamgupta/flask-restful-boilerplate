531feece44e7726f4e47b90bb68788b5df014a4a
Kamya <kamya_Acc@Kamyas-MacBook-Air.local>
2021-02-27 17:56:42 +0530

    generalized config

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@127.0.0.1:3307/db_user_microservice"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Medicaps1!@127.0.0.1/db_user_microservice"
+#SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@127.0.0.1:3307/db_user_microservice"
+#SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root@127.0.0.1:3306/db_user_microservice"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
